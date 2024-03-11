d91136323c30a107a3c404eb4d97b781ebf5b95c
@m.antonovich <m.antonovich@thirdpin.io>
2024-03-11 22:12:52 +0300

    [fix] 101, 104 ex

---
+++ leetcode/ex104.py
@@ -0,0 +1,26 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def maxDepth(root):
+    if not root:
+        return 0
+    else:
+        left_depth = maxDepth(root.left)
+        right_depth = maxDepth(root.right)
+        return max(left_depth, right_depth) + 1
+
+# Example 1
+root1 = TreeNode(3)
+root1.left = TreeNode(9)
+root1.right = TreeNode(20)
+root1.right.left = TreeNode(15)
+root1.right.right = TreeNode(7)
+print(maxDepth(root1))  # Output: 3
+
+# Example 2
+root2 = TreeNode(1)
+root2.right = TreeNode(2)
+print(maxDepth(root2))  # Output: 2
